b6d45c3ba797fbb18cd380225872337bc70f94da
gioiabaldissin <kicasta@gmail.com>
2018-04-24 14:16:21 +0200

    added tokenize method stub

--- src/main/java/Actions/Tokenizer.java
+++ src/main/java/Actions/Tokenizer.java
@@ -1,8 +1,19 @@
 package Actions;
 
+import Entities.Token;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.List;
+
 /**
  * Created by baldissin - 24/4/18.
  */
 public class Tokenizer {
+
+    public static List<Token> tokenize(List<String> terms)
+    {
+        //TODO: Method Stub
+        throw new NotImplementedException();
+    }
 
 }
